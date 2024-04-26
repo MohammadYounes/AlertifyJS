5e15b191ecde6d52667cdfbe0b61c7a9a6ae9fef
Mohammad Younes <myounes@gmail.com>
2024-04-26 22:03:51 +0300

    1.14.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function (grunt) {
             options: {
                 map: false,
                 processors: [
-                    require('autoprefixer')({browsers: 'last 2 versions'}),
+                    require('autoprefixer')(),
                 ]
             },
             build: {
